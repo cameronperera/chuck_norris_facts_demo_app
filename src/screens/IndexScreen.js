4e21ff705b371ebb10bf360e19e3f6b8557c2e94
Cameron Perera <cameron.perera@samaritanministries.org>
2020-07-15 09:39:56 -0500

    Add Chuck Norris picture

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, Button, Image } from 'react-native'
 import { Context } from '../context/JokeContext';
 
 const IndexScreen = ({ navigation }) => {
@@ -10,13 +10,12 @@ const IndexScreen = ({ navigation }) => {
   }, [])
   // componentDidMount, DidUpdate, WillUnmount
 
-
-  // todo: insert great chuck pic
     return (
     <View>
+      <Image style={styles.image} source={{ uri: 'https://images02.military.com/sites/default/files/media/veteran-jobs/content-images/2016/03/chucknorris.jpg'}} />
       <Text style={styles.title}>{state.jokeOfTheDay && state.jokeOfTheDay.joke}</Text>
       <Button
-        title='Make your own joke!'
+        title='Become the joke!'
         onPress={() => navigation.navigate('CustomJoke')}
       />
     </View>
